20ad4e2d72715ce33caa44e7acaf803abb51f516
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-07-15 17:24:29 +0200

    Stop using NPM

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,4 @@
-import Reveal from './node_modules/reveal.js/js/index.js';
-import Markdown from './node_modules/reveal.js/plugin/markdown/plugin.js';
-
 Reveal.initialize({
   hash: true,
-  plugins: [Markdown],
+  plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ],
 });
